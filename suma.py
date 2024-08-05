515884d59c2ea43f7be28ff6755afd95f2ccc7a3
Jhon Forero <jhon.forero01@usc.edu.co>
2024-08-04 23:34:57 -0500

    Agregando archivo

---
+++ suma.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+
+def suma(num1, num2):
+    return num1 + num2
+
+print('La suma es igual a: ' + str(suma(numero2,numero1)))
